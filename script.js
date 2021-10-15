07f034d284ef01a6bbb2f845e826478131afc6b0
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-15 14:04:17 -0400

    add random char functions

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+
 // Series of Prompts
 // User will be asked first what length they would like their password to be
 // Send an error message if outside range
@@ -31,7 +33,25 @@ if (userpwLength > 128 || userpwLength < 8) {
 if (userpwUpper + userpwLower + userpwNumeric + userpwSpecial === 0) {
   window.alert("Please select at least one character type");
 }
+// create Random values to be used in password creation
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumeric() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSpecial() {
+        const special = ['\u0020','\u0021','\u0022','\u0023','\u0024','\u0025', '\u0026','\u0027', '\u0028', '\u0029', '\u002A','\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u005B', '\u005C', '\u005D', '\u005E', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
+        return special[Math.floor(Math.random() * special.length)];
+}
+//source of special charaters unicode https://owasp.org/www-community/password-special-characters
+//use the unicodes for special characters to avoid syntax problems in the array
 
 
 // Write password to the #password input
@@ -45,3 +65,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Call writePassword function
+writePassword();
